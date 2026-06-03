52841be287115b760174646505aeafc2133dcbae
bilguung016-oss <bilguung016@gmail.com>
2026-06-03 16:59:06 +0800

    Use PostgreSQL on Railway for persistent data

--- app.py
+++ app.py
@@ -6,7 +6,10 @@ import json
 
 app = Flask(__name__)
 app.secret_key = os.environ.get('SECRET_KEY', os.urandom(24))
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///survey.db')
+db_url = os.environ.get('DATABASE_URL', 'sqlite:///survey.db')
+if db_url.startswith('postgres://'):
+    db_url = db_url.replace('postgres://', 'postgresql://', 1)
+app.config['SQLALCHEMY_DATABASE_URI'] = db_url
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
